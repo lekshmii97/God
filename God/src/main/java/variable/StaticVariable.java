73b3aface8d9b15562ef4f19942272693a9cfe0a
Zartech <Zartech@DESKTOP-CN5728E>
2023-04-22 13:02:36 +0530

    Static

---
+++ God/src/main/java/variable/StaticVariable.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class StaticVariable {
+	static int a=10;
+	static int b=20;
+	static int c;
+
+	public static void main(String[] args) {
+	c=a+b;
+	System.out.println(c);
+
+	}
+
+}
