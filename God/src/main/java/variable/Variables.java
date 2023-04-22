91c164d0b43cb5b8888a21b68c94aa7e269b0873
Zartech <Zartech@DESKTOP-CN5728E>
2023-04-22 12:58:49 +0530

    variable

---
+++ God/src/main/java/variable/Variables.java
@@ -0,0 +1,28 @@
+package variable;
+
+public class Variables {
+
+	
+		
+		int a=10;
+		static String name="God";
+		public static void main(String[] args) {
+			
+			int b=20;
+			System.out.println("name="+name);
+
+		Variables obj=new Variables();
+		System.out.println(obj.a);
+		System.out.println("b="+b);
+			
+		
+			}
+		
+		
+
+
+
+
+	}
+
+
