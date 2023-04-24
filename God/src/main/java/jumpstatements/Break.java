6d5ee7a46895171db7962ad9f55756ffa50bcd95
Zartech <Zartech@DESKTOP-CN5728E>
2023-04-24 19:17:42 +0530

    jump

---
+++ God/src/main/java/jumpstatements/Break.java
@@ -0,0 +1,17 @@
+package jumpstatements;
+
+public class Break {
+
+	public static void main(String[] args) {
+		for(int a=1;a<=10;a++)
+		{
+			if(a==5)
+			{
+				break;
+			}
+			System.out.println(a);
+		}
+
+	}
+
+}
