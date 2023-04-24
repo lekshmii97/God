1785591a1dc8397bfe6820af27c19646643e60a7
Zartech <Zartech@DESKTOP-CN5728E>
2023-04-24 19:17:29 +0530

    loop statements

---
+++ God/src/main/java/loopstatements/DoWhile.java
@@ -0,0 +1,17 @@
+package loopstatements;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		int a=1;
+	do
+	{
+		System.out.println(a);
+		a++;
+	}
+	while(a<=10);
+
+	}
+
+}
---
+++ God/src/main/java/loopstatements/NestedLoop.java
@@ -0,0 +1,16 @@
+package loopstatements;
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++)
+
+		{
+			for(int j=1;j<4;j++)
+			{
+				System.out.println(i+" "+j);
+			}
+		}
+	}
+
+}
