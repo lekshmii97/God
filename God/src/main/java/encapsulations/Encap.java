71b12748a68bbac53b009ea13c5e0dbf1111b25d
Zartech <Zartech@DESKTOP-CN5728E>
2023-05-02 09:41:18 +0530

    Encap

---
+++ God/src/main/java/encapsulations/Encap.java
@@ -0,0 +1,22 @@
+package encapsulations;
+
+public class Encap {
+
+
+		private String name;
+		private int age;
+		
+	public void setmethod()
+	{
+		name="lekshmi";
+		age=25;
+	}
+public void getmethod()
+{
+	System.out.println(name);
+	System.out.println(age);
+}
+	
+
+
+}
---
+++ God/src/main/java/encapsulations/Encap1.java
@@ -0,0 +1,12 @@
+package encapsulations;
+
+public class Encap1 extends Encap{
+
+	public static void main(String[] args) {
+		Encap ob=new Encap();
+		ob.setmethod();
+		ob.getmethod();
+
+	}
+
+}
